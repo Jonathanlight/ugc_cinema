7f703adeeaac1f94c1fdbfd69df06867b16d695b
Jonathan <Jonathan.kablan@gmail.com>
2020-12-19 18:06:39 +0100

    menu user

--- apps/user/favorie.php
+++ apps/user/favorie.php
@@ -1,5 +1,5 @@
 <?php include_once('../../composants/dashboard/header.php') ?>
-<?php include_once('../../composants/dashboard/menu.php') ?>
+<?php include_once('../../composants/dashboard/menu_user.php') ?>
 
     <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
       <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
