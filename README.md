3880ab8cd3932ee2ad7f9ed3dcc941e9b586a138
Jonathan <Jonathan.kablan@gmail.com>
2020-12-19 18:42:26 +0100

    readme

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # ugc_cinema
 
-
 - PHP
 - MYSQL
 - BOOTSTRAP
 - HTML/CSS
+
+![drawing](assets/images/screen.png)
--- apps/user/profil.php
+++ apps/user/profil.php
@@ -5,7 +5,7 @@
       <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">Profil</h1>
       </div>
-	    
+
     </main>
   </div>
 </div>
